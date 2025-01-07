bd07bb3a5b478bbb5ead12e77f0ae900dbf4482b
Albert Ivanov <albertivanov187@gmail.com>
2025-01-07 13:46:23 +0200

    Add project files.

---
+++ 01. BinaryDigitsCount/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+byte number2 = byte.Parse(Console.ReadLine());
+
+int counter = 0;
+while (number != 0)
+{
+	int remainder = number % 2;
+
+	if (remainder == number2)
+	{
+		counter++;
+	}
+
+	number /= 2;
+}
+
+Console.WriteLine(counter);
