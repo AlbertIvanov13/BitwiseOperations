0dfa2848730d91e6db3317a0c739257d3d5b114a
Albert Ivanov <albertivanov187@gmail.com>
2025-01-07 13:59:05 +0200

    100/100

---
+++ 02. BitAtPosition1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+
+List<int> bits = new List<int>();
+
+while (number != 0)
+{
+	int remainder = number % 2;
+	bits.Add(remainder);
+
+	number /= 2;
+}
+
+for (int i = bits.Count - 1; i >= 0; i--)
+{
+	Console.WriteLine(bits[1]);
+	break;
+}
