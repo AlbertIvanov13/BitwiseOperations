f1ebae94038214c692a465c6aed42f51f3d3625a
Albert Ivanov <albertivanov187@gmail.com>
2025-01-07 17:42:16 +0200

    100/100

---
+++ 02.1. BitAtPosition1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int newNumber = n;
+int position = 1;
+int bitAtPosition1 = 0;
+
+List<int> bits = new List<int>();
+while (n != 0)
+{
+	int remainder = n % 2;
+	bits.Add(remainder);
+
+	n /= 2;
+}
+
+
+bitAtPosition1 = (newNumber >> position) & 1;
+
+for (int i = 0; i < bits.Count; i++)
+{
+	if (bitAtPosition1 == 1)
+	{
+		bitAtPosition1 = 1;
+		break;
+	}
+	else
+	{
+		bitAtPosition1 = 0;
+		break;
+	}
+}
+
+Console.WriteLine(bitAtPosition1);
