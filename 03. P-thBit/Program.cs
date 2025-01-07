ae6f8dfcdecf343f7e0437c9e15810a1dd154b11
Albert Ivanov <albertivanov187@gmail.com>
2025-01-07 21:08:31 +0200

    100/100 :D

---
+++ 03. P-thBit/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int newNumber = n;
+int p = int.Parse(Console.ReadLine());
+int bitAtPositionP = 0;
+
+List<int> remainders = new List<int>();
+while (n != 0)
+{
+	int remainder = n % 2;
+	remainders.Add(remainder);
+
+	n /= 2;
+}
+
+bitAtPositionP = (newNumber >> p) & 1;
+Console.WriteLine(bitAtPositionP);
