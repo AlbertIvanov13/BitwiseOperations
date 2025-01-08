b45e9b9f0c6344b30cf525ac2188867ccf34f0f0
Albert Ivanov <albertivanov187@gmail.com>
2025-01-08 14:37:27 +0200

    100/100 :D

---
+++ 04. BitDestroyer/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+int mask = 0;
+int newNumber = 0;
+
+mask = ~(1 << p);
+
+newNumber = n & mask;
+
+Console.WriteLine(newNumber);
