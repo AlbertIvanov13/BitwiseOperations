89533e524621e73d43c34947e2f17c64ec36bbd5
Albert Ivanov <albertivanov187@gmail.com>
2025-01-08 16:34:50 +0200

    100/100 :D

---
+++ 06. Tri-bitSwitch/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+
+int mask = 0;
+int newNumber = 0;
+
+string binary = Convert.ToString(n, 2).PadLeft(32, '0');
+
+mask = (7 << p);
+
+string binary2 = Convert.ToString(mask, 2).PadLeft(32, '0');
+
+newNumber = n ^ mask;
+
+string binary3 = Convert.ToString(newNumber, 2).PadLeft(32, '0');
+
+Console.WriteLine(newNumber);
