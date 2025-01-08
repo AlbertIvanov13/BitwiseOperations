c2204bd93bc395e393e7d57db483b4cc10554552
Albert Ivanov <albertivanov187@gmail.com>
2025-01-08 15:57:42 +0200

    100/100

---
+++ 05. OddTimes/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+	string binary1 = Convert.ToString(result, 2).PadLeft(8, '0');
+	string binary2 = Convert.ToString(numbers[i], 2).PadLeft(8, '0');
+
+	result = result ^ numbers[i];
+	string binary3 = Convert.ToString(result, 2).PadLeft(8, '0');
+}
+  
+Console.WriteLine(result);
